759106643b8adc0c1bb5a61c7aa0ba1d7aa06e16
Sughosh Kadkol <blufugen@gmail.com>
2018-11-15 15:27:45 -0800

    Removing the backup of the broken mac Sandbox

--- Public/src/Sandbox/MacOs_old/scripts/kext-log.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-log show --last 1m --predicate 'eventMessage contains "domino_Sandbox"' | grep "domino_Sandbox" | sed 's/^.*domino_Sandbox ]]//g' "$@"
-
--- Public/src/Sandbox/MacOs_old/tools/iosimulator/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-dotnet publish -c Release

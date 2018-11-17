b78d2e6428c22518d1050947abc4de771a6b3a18
Sughosh Kadkol <blufugen@gmail.com>
2018-11-16 17:46:58 -0800

    Add a helpful hint about disabling SIP

--- macbuild.sh
+++ macbuild.sh
@@ -16,3 +16,7 @@ done
 
 dotnet build BuildXL.MacSandbox.sln --configuration $arg_conf \
     && BUILDXL_BIN=bin/$arg_conf Public/src/Sandbox/MacOs/scripts/build.sh $arg_conf --load-kext
+
+if [ $? -ne 0 ]; then
+	echo -e "------\n If the build succeeds but Kext installation fails, disable SIP. See https://www.imore.com/how-turn-system-integrity-protection-macos \n------"
+fi

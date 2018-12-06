9e587c4e8ad634a2c328cc705aeb2c13126a18e8
Sughosh Kadkol <sukadkol@microsoft.com>
2018-12-06 14:37:22 -0800

    Mac sandbox does not relay the process parent IDs required to make ProcessTree work. Converting ProcessTree demo into a simpler SandboxedProcessLister demo.

--- Public/src/Demos/ProcessTree/SandboxedProcessReporter.cs
+++ Public/src/Demos/ProcessTree/SandboxedProcessReporter.cs
@@ -2,10 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Diagnostics.ContractsLight;
 using System.IO;
-using System.Linq;
 using BuildXL.Processes;
 using BuildXL.Utilities;
 using BuildXL.Utilities.Instrumentation.Common;
@@ -15,16 +12,16 @@ namespace BuildXL.Demo
     /// <summary>
     /// Runs a given process under the sandbox and builds a (historical) process tree based on the execution
     /// </summary>
-    public class ProcessTreeBuilder
+    public class SandboxedProcessReporter
     {
         private readonly LoggingContext m_loggingContext;
         private readonly PathTable m_pathTable;
 
         /// <nodoc/>
-        public ProcessTreeBuilder()
+        public SandboxedProcessReporter()
         {
             m_pathTable = new PathTable();
-            m_loggingContext = new LoggingContext(nameof(ProcessTreeBuilder));
+            m_loggingContext = new LoggingContext(nameof(SandboxedProcessReporter));
         }
 
         /// <summary>
@@ -34,47 +31,11 @@ namespace BuildXL.Demo
         /// <remarks>
         /// Each reported process in the tree contains process data, such as start/end time, arguments, CPU counters, etc.
         /// </remarks>
-        public ProcessTree RunProcessAndReportTree(string pathToProcess, string arguments)
+        public IReadOnlyList<ReportedProcess> RunProcessAndReportTree(string pathToProcess, string arguments)
         {
             var result = RunProcessUnderSandbox(pathToProcess, arguments);
             // The sandbox reports all processes as a list. Let's make them a tree for better visualization.
-            return ComputeTree(result.Processes);
-        }
-
-        private ProcessTree ComputeTree(IReadOnlyCollection<ReportedProcess> reportedProcesses)
-        {
-            // Process Ids can be reused, but let's assume uniqueness, just for demo purposes
-            var allProcessNodes = reportedProcesses.ToDictionary(proc => proc.ProcessId, proc => new ProcessNode(proc));
-
-            ProcessNode root = null;
-            foreach (var reportedProcess in reportedProcesses)
-            {
-                // If the parent id is part of the reported processes, update the children. Otherwise, that should be the root process
-                if (allProcessNodes.TryGetValue(reportedProcess.ParentProcessId, out ProcessNode parentProcessNode))
-                {
-                    parentProcessNode.AddChildren(allProcessNodes[reportedProcess.ProcessId]);
-                }
-                else if (reportedProcess.ParentProcessId == 0)
-                {
-                    // This case means that the parent process id could not be retrieved. Just ignore the process for sake of the demo
-                    continue;
-                }
-                else
-                {
-                    // There should be only one process that has a parent id that is not part of the set of reported processes: the main one
-                    root = allProcessNodes[reportedProcess.ProcessId];
-                }
-            }
-
-            // Seal all nodes so children become immutable
-            foreach (var processNode in allProcessNodes.Values)
-            {
-                processNode.Seal();
-            }
-
-            Contract.Assert(root != null);
-
-            return new ProcessTree(root);
+            return result.Processes;
         }
 
         /// <summary>

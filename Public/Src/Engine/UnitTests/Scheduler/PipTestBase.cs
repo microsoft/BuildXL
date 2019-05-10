1acc262ee298820b652e40fc47964f429daf249f
Aleksandar Milicevic <almili@microsoft.com>
2019-05-10 14:28:19 -0700

    [macOS] Compile build tools to native (#285)
    Additional tools compiled to native:
    
     - ResXPreprocessor
     - LogGen
     - TestProcess
     - OpNameGenerator

--- Public/Src/Engine/UnitTests/Scheduler/PipTestBase.cs
+++ Public/Src/Engine/UnitTests/Scheduler/PipTestBase.cs
@@ -213,6 +213,17 @@ namespace Test.BuildXL.Scheduler
                 builder.ToolDescription = StringId.Create(Context.StringTable, description);
             }
 
+            if (OperatingSystemHelper.IsUnixOS)
+            {
+                builder.SetEnvironmentVariable(
+                    StringId.Create(Context.StringTable, "DYLD_LIBRARY_PATH"),
+                    Path.GetDirectoryName(TestProcessExecutable.Path.ToString(Context.PathTable)));
+
+                // untracking this directory as well because dynamic probes are non-deterministic which
+                // can cause some of our FingerprintStore tests to fail.
+                builder.AddUntrackedDirectoryScope(TestProcessExecutable.Path.GetParent(Context.PathTable));
+            }
+
             return builder;
         }
 

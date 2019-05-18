361f5dff80ce50f5c50e33e165098a652486cb69
Erik Mavrinac <erikmav@microsoft.com>
2019-05-17 23:38:21 -0700

    Enable UT

--- Public/Src/Engine/UnitTests/Processes.Detours/SubstituteProcessExecutionTests.cs
+++ Public/Src/Engine/UnitTests/Processes.Detours/SubstituteProcessExecutionTests.cs
@@ -132,7 +132,7 @@ namespace Test.BuildXL.Processes.Detours
         /// </summary>
         [Theory]
         [InlineData(false, null)]
-// TODO: erikmav filtering broken generally       [InlineData(true, "foo.exe")]  // Filter should not match
+y       [InlineData(true, "foo.exe")]  // Filter should not match
         public async Task CmdWithTestShim_ShimNothingRunsChildProcessWithoutShim(bool shimAllProcesses, string processMatch)
         {
             var context = BuildXLContext.CreateInstanceForTesting();

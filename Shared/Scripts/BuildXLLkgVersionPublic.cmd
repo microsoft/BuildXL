e5f345c1ac5d77065b1cf82a7fa08ec0e41c2eae
CloudDev Build Account <tsebuild@microsoft.com>
2019-05-22 22:29:52 +0000

    New Public LKG 0.1.0-20190521.5

--- Shared/Scripts/BuildXLLkgVersionPublic.cmd
+++ Shared/Scripts/BuildXLLkgVersionPublic.cmd
@@ -1,5 +1,5 @@
 ﻿REM @echo off
 REM TODO: update LKG_NAME to win-x64 once we start publishing that nuget to MyGet
 set BUILDXL_LKG_NAME=Microsoft.BuildXL.net472
-set BUILDXL_LKG_VERSION=0.1.0-20190520.2
+set BUILDXL_LKG_VERSION=0.1.0-20190521.5
 set BUILDXL_LKG_FEED_1=https://dotnet.myget.org/F/buildxl-selfhost/api/v3/index.json

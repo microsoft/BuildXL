68350de34918a80eca4cab739e545de31b52e832
Aleksandar Milicevic <almili@microsoft.com>
2019-05-22 16:53:58 -0700

    Switch public LKG to win-x64

--- Shared/Scripts/BuildXLLkgVersionPublic.cmd
+++ Shared/Scripts/BuildXLLkgVersionPublic.cmd
@@ -1,5 +1,4 @@
 ﻿REM @echo off
-REM TODO: update LKG_NAME to win-x64 once we start publishing that nuget to MyGet
-set BUILDXL_LKG_NAME=Microsoft.BuildXL.net472
+set BUILDXL_LKG_NAME=Microsoft.BuildXL.win-x64
 set BUILDXL_LKG_VERSION=0.1.0-20190521.5
 set BUILDXL_LKG_FEED_1=https://dotnet.myget.org/F/buildxl-selfhost/api/v3/index.json

bd4ed1f2871b42ee2b7c2870a5f085499f9b9076
Iman Narasamdya <imnarasa@microsoft.com>
2019-06-03 12:32:17 -0700

    Flight consistent temp directory creation behind a flag (#398)
    Changes that made temp directory creation consistent break customers. Here, we put it (hopefully temporarily) behind a flag

--- Public/Src/App/Bxl/Args.cs
+++ Public/Src/App/Bxl/Args.cs
@@ -359,6 +359,9 @@ namespace BuildXL
                         OptionHandlerFactory.CreateBoolOption(
                             "enableGrpc",
                             sign => distributionConfiguration.IsGrpcEnabled = sign),
+                        OptionHandlerFactory.CreateBoolOption(
+                            "enableIncrementalFrontEnd",
+                            sign => frontEndConfiguration.EnableIncrementalFrontEnd = sign),
                         OptionHandlerFactory.CreateBoolOptionWithValue(
                             "enableLazyOutputs",
                             (opt, sign) => HandleLazyOutputMaterializationOption(opt, sign, schedulingConfiguration)),
@@ -366,11 +369,8 @@ namespace BuildXL
                             "engineCacheDirectory",
                             opt => layoutConfiguration.EngineCacheDirectory = CommandLineUtilities.ParsePathOption(opt, pathTable)),
                         OptionHandlerFactory.CreateBoolOption(
-                            "enableIncrementalFrontEnd",
-                            sign => frontEndConfiguration.EnableIncrementalFrontEnd = sign),
-                        OptionHandlerFactory.CreateBoolOption(
-                            "respectWeakFingerprintForNugetUpToDateCheck",
-                            sign => frontEndConfiguration.RespectWeakFingerprintForNugetUpToDateCheck = sign),
+                            "ensureTempDirectoriesExistenceBeforePipExecution",
+                            sign => sandboxConfiguration.EnsureTempDirectoriesExistenceBeforePipExecution = sign),
                         OptionHandlerFactory.CreateOption(
                             "environment",
                             opt => loggingConfiguration.Environment = CommandLineUtilities.ParseEnumOption<ExecutionEnvironment>(opt)),
@@ -715,7 +715,7 @@ namespace BuildXL
                             "redirectUserProfile",
                             opt => enableProfileRedirect = opt),
                         OptionHandlerFactory.CreateOption(
-                            "RedirectedUserProfileJunctionRoot",
+                            "redirectedUserProfileJunctionRoot",
                             opt => layoutConfiguration.RedirectedUserProfileJunctionRoot = CommandLineUtilities.ParsePathOption(opt, pathTable)),
                         OptionHandlerFactory.CreateOption(
                             "relatedActivityId",
@@ -735,6 +735,9 @@ namespace BuildXL
                         OptionHandlerFactory.CreateBoolOption(
                             "replicateOutputsToWorkers",
                             sign => distributionConfiguration.ReplicateOutputsToWorkers = sign),
+                        OptionHandlerFactory.CreateBoolOption(
+                            "respectWeakFingerprintForNugetUpToDateCheck",
+                            sign => frontEndConfiguration.RespectWeakFingerprintForNugetUpToDateCheck = sign),
                         OptionHandlerFactory.CreateBoolOption(
                             "reuseEngineState",
                             sign => engineConfiguration.ReuseEngineState = sign),

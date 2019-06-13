3bfe7110e4df709e5a4b7d7ebd697afde5b24fdf
Juan Carlos Guzman <juguzman@microsoft.com>
2019-06-13 09:26:06 -0700

    Remove unnecessary using statement

--- Public/Src/Cache/ContentStore/Interfaces/Stores/ContentHashWithLastAccessTimeAndReplicaCount.cs
+++ Public/Src/Cache/ContentStore/Interfaces/Stores/ContentHashWithLastAccessTimeAndReplicaCount.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Security.Cryptography.X509Certificates;
 using BuildXL.Cache.ContentStore.Hashing;
 
 namespace BuildXL.Cache.ContentStore.Interfaces.Stores

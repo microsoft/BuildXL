22518844b39b031eb34d835e2e1816d94e02cd23
Juan Carlos Guzman <juguzman@microsoft.com>
2019-07-15 09:13:02 -0700

    Remove unnecessary using statement

--- Public/Src/Cache/ContentStore/Distributed/NuCache/TransitioningContentLocationStore.cs
+++ Public/Src/Cache/ContentStore/Distributed/NuCache/TransitioningContentLocationStore.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.ContractsLight;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using BuildXL.Cache.ContentStore.Distributed.Redis;

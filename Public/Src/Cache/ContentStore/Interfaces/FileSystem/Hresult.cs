ccb7ede26de82eb7faa41f1b36896621412da932
Juan Carlos Guzman <juguzman@microsoft.com>
2019-07-19 09:57:15 -0700

    Stop retrying on disk full errors

--- Public/Src/Cache/ContentStore/Interfaces/FileSystem/Hresult.cs
+++ Public/Src/Cache/ContentStore/Interfaces/FileSystem/Hresult.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace BuildXL.Cache.ContentStore.Interfaces.FileSystem
 {
     /// <summary>
@@ -17,5 +19,12 @@ namespace BuildXL.Cache.ContentStore.Interfaces.FileSystem
         ///     FILE_EXISTS
         /// </summary>
         public const uint FileExists = 0x80070050;
+
+        /// <summary>
+        ///     ERROR_DISK_FULL
+        /// </summary>
+        public const uint DiskFull = 0x80070070;
+
+        public static bool HasHresult(this Exception e, uint hresult) => ((uint)e.HResult) == hresult;
     }
 }

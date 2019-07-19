9191ccc01bbc8f670a715f6b2fae2d68a3bc7ad6
Michael Pysson <mpysson@microsoft.com>
2019-07-19 12:39:34 -0700

    Make legacy test work and fix violations

--- Public/Src/App/UnitTests/Bxl/BuildXLAppTests.cs
+++ Public/Src/App/UnitTests/Bxl/BuildXLAppTests.cs
@@ -1,19 +1,19 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Reflection;
+using System.Text;
+using System.Xml;
 using BuildXL;
 using BuildXL.App.Tracing;
 using BuildXL.Utilities;
+using BuildXL.Utilities.Configuration;
 using BuildXL.Utilities.Instrumentation.Common;
 using BuildXL.Utilities.Tracing;
-using BuildXL.Utilities.Configuration;
 using Test.BuildXL.TestUtilities.Xunit;
 using Xunit;
-using System.Collections.Generic;
-using System.Xml;
-using System.Reflection;
-using System.Diagnostics.Tracing;
-using System;
 
 namespace Test.BuildXL
 {
@@ -112,79 +112,24 @@ namespace Test.BuildXL
         }
 
         /// <summary>
-        /// Having more than one event with the same EventId will cause chaos. Make sure it doesn't happen
+        /// Having more than one event with the same EventId will cause chaos. Make sure it doesn't happen.
         /// </summary>
-        /// <remarks>
-        /// This test leverage's EventSource's support for generating a manifes file to get all of the possible events
-        /// that could be logged. This is maybe a bit roundabout but it the legacy of a time when an ETW manifest file
-        /// was generated as part of the core product
-        /// </remarks>
         [Fact]
         public void EventIdsDontOverlap()
         {
-            TestEventListener testEventListener = new TestEventListener(Events.Log, "ThisTest");
-            Events.Log.VerboseEvent("asdf");
-            System.Diagnostics.Debugger.Launch();
+            Dictionary<string, string> allEncounteredEvents = new Dictionary<string, string>();
+            StringBuilder errors = new StringBuilder();
 
-            // Track the guids of all providers to create combined list
-            List<Guid> providers = new List<Guid>();
-
-            // First we load the original event source. We will then merge all other manfests into this file
-            XmlDocument outerDoc = new XmlDocument();
-            var type = Events.Log.GetType();
-            var location = Assembly.GetAssembly(Events.Log.GetType()).Location;
-            outerDoc.LoadXml(EventSource.GenerateManifest(Events.Log.GetType(), Assembly.GetAssembly(Events.Log.GetType()).Location));
-            XmlNamespaceManager manager = new XmlNamespaceManager(outerDoc.NameTable);
-            manager.AddNamespace("x", outerDoc.DocumentElement.NamespaceURI);
-
-            providers.Add(Events.Log.Guid);
-
-            // Nodes used as markers to merge other nodes
-            XmlNode outerProvider = outerDoc.SelectSingleNode("//x:provider", manager);
-            XmlNode outerStringTable = outerDoc.SelectSingleNode("//x:stringTable", manager);
-
-            // Each additional manifest gets merged into the primary one
+            // Look at each generated event source to make sure it doesn't have any conflicts
             foreach (var eventSource in global::BuildXL.BuildXLApp.GeneratedEventSources)
             {
-                providers.Add(eventSource.Guid);
+                int startCount = allEncounteredEvents.Count;
                 XmlDocument doc = new XmlDocument();
                 doc.LoadXml(EventSource.GenerateManifest(eventSource.GetType(), Assembly.GetAssembly(eventSource.GetType()).Location));
+                XmlNamespaceManager mgr = new XmlNamespaceManager(doc.NameTable);
+                mgr.AddNamespace("x", doc.DocumentElement.NamespaceURI);
 
-                XmlNode provider = doc.SelectSingleNode("//x:provider", manager);
-                XmlNode stringTable = doc.SelectSingleNode("//x:stringTable", manager);
-
-                if (provider != null)
-                {
-                    outerProvider.ParentNode.AppendChild(outerDoc.ImportNode(provider, true));
-                }
-
-                if (stringTable != null)
-                {
-                    foreach (XmlNode entry in stringTable.ChildNodes)
-                    {
-                        outerStringTable.AppendChild(outerDoc.ImportNode(entry, true));
-                    }
-                }
-            }
-
-            using (MemoryStream ms = new MemoryStream())
-            {
-                outerDoc.Save(ms);
-                ms.Position = 0;
-                StreamReader reader = new StreamReader(ms);
-                string manifestData = reader.ReadToEnd();
-                XmlDocument newDoc = new XmlDocument();
-                newDoc.LoadXml(manifestData.Replace(" xmlns=", " nope="));
-
-                Dictionary<string, string> events = new Dictionary<string, string>();
-
-                var nodeSet = newDoc.SelectNodes(@"//event");
-                if (nodeSet == null || nodeSet.Count == 1)
-                {
-                    XAssert.Fail("Couldn't find any events. Test must be broken");
-                }
-
-                foreach (XmlNode node in nodeSet)
+                foreach (XmlNode node in doc.SelectNodes(@"//x:event", mgr))
                 {
                     string eventId = node.Attributes["value"].Value;
                     string eventName = node.Attributes["symbol"].Value;
@@ -197,15 +142,25 @@ namespace Test.BuildXL
                     }
 
                     string oldEventName;
-                    if (events.TryGetValue(eventId, out oldEventName))
+                    if (allEncounteredEvents.TryGetValue(eventId, out oldEventName))
                     {
-                        XAssert.Fail("error: encountered duplicate event id:{0}. Existing Event:{1}, New Event:{2}", eventId, oldEventName, eventName);
+                        errors.AppendLine($"error: encountered duplicate event id:{eventId}. Existing Event:{oldEventName}, New Event:{eventName}");
                     }
                     else
                     {
-                        events.Add(eventId, eventName);
+                        allEncounteredEvents.Add(eventId, eventName);
                     }
                 }
+
+                if (allEncounteredEvents.Count == startCount)
+                {
+                    errors.AppendLine($"Didn't found any events in EventSource: {eventSource.GetType().ToString()}");
+                }
+            }
+
+            if (errors.Length != 0)
+            {
+                XAssert.Fail(errors.ToString());
             }
         }
     }

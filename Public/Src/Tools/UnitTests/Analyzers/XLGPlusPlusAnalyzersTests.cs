6805d5ba897187db3eab460ba9fa9253b984292f
Sahil Gandhi <t-sagand@microsoft.com>
2019-07-19 16:55:13 -0700

    Init unit test for xlg++

---
+++ Public/Src/Tools/UnitTests/Analyzers/XLGPlusPlusAnalyzersTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuildXL.Execution.Analyzer;
+using Xunit.Abstractions;
+
+namespace Test.Tool.Analyzers
+{
+    /// <summary>
+    /// Tests for <see cref="XLGToDBAnalyzer"/>
+    /// 
+    /// </summary>
+    public class XLGPlusPlusAnalyzersTests : AnalyzerTestBase
+    {
+        public XLGPlusPlusAnalyzersTests(ITestOutputHelper output): base(output)
+        {
+
+        }
+    }
+}

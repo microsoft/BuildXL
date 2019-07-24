49d765dc80ada01afa8555e0a299b6de9c9bcdea
Sahil Gandhi <t-sagand@microsoft.com>
2019-07-24 11:14:25 -0700

    Rename file and fix with oleksiis comments

--- Public/Src/Tools/UnitTests/Analyzers/XLGToDBAnalyzerTests.cs
+++ Public/Src/Tools/UnitTests/Analyzers/XLGToDBAnalyzerTests.cs
@@ -2,21 +2,13 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.IO;
-using BuildXL.Pips.Builders;
-using BuildXL.Pips.Operations;
-using BuildXL.Scheduler;
+using BuildXL.Execution.Analyzer;
 using BuildXL.Utilities;
 using Test.BuildXL.Executables.TestProcess;
-using Test.BuildXL.Scheduler;
 using Test.BuildXL.TestUtilities.Xunit;
-using BuildXL.Execution.Analyzer;
 using Xunit;
 using Xunit.Abstractions;
 using static BuildXL.ToolSupport.CommandLineUtilities;
-using static Test.Tool.Analyzers.AnalyzerTestBase;
-using BuildXLConfiguration = BuildXL.Utilities.Configuration;
-using System;
-using System.Diagnostics;
 
 namespace Test.Tool.Analyzers
 {
@@ -25,11 +17,10 @@ namespace Test.Tool.Analyzers
     /// The construction and disposal of these tests rely on the fact that 
     /// Xunit uses a unique class instance for each test (as is with the other Analyzer Tests).
     /// </summary>
-    public class XLGPlusPlusAnalyzersTests : AnalyzerTestBase
+    public class XLGToDBAnalyzerTests : AnalyzerTestBase
     {
-
         private AbsolutePath DirPath { get; set; }
-        public XLGPlusPlusAnalyzersTests(ITestOutputHelper output): base(output)
+        public XLGToDBAnalyzerTests(ITestOutputHelper output) : base(output)
         {
             AnalysisMode = AnalysisMode.XlgToDb;
 
@@ -46,7 +37,10 @@ namespace Test.Tool.Analyzers
         }
 
         /// <summary>
-        /// This test makes sure that a RocksDB database was created and populated with some sst files.
+        /// This test makes sure that a RocksDB database was created. 
+        /// If a DB is created, and events have been populated, one or more sst 
+        /// files will also be created, and this test makes sure there is at least 
+        /// such sst file that is present.
         /// </summary>
         [Fact]
         public void DBCreatedAndPopulated()
@@ -55,12 +49,12 @@ namespace Test.Tool.Analyzers
 
             var file = FileArtifact.CreateOutputFile(Combine(DirPath, "blah.txt"));
 
-            var pipB = CreateAndSchedulePipBuilder(new Operation[]
+            var pipA = CreateAndSchedulePipBuilder(new Operation[]
             {
                 Operation.WriteFile(file),
             }).Process;
 
-            var pipC = CreateAndSchedulePipBuilder(new Operation[]
+            var pipB = CreateAndSchedulePipBuilder(new Operation[]
             {
                 Operation.ReadFile(file),
                 Operation.WriteFile(CreateOutputFileArtifact())
@@ -70,16 +64,7 @@ namespace Test.Tool.Analyzers
 
             var analyzerRes = RunAnalyzer(buildA).AssertSuccess();
 
-            XAssert.AreNotEqual(Directory.GetFiles(DirPath.ToString(Context.PathTable), "*.sst"), 0);
-        }
-
-        /// <summary>
-        /// TODO: Once all events can be successfully stored, make sure that the count in xlg = count in DB
-        /// </summary>
-        [Fact]
-        public void ValidateEventCount()
-        {
-            XAssert.AreEqual(5, 5);
+            XAssert.AreNotEqual(Directory.GetFiles(DirPath.ToString(Context.PathTable), "*.sst").Length, 0);
         }
     }
 }

4685001c6076b9d4e71f83aab48e7d3d479cc6db
CloudDev Build Account <tsebuild@microsoft.com>
2019-08-01 21:47:49 +0000

    New Public LKG 0.1.0-20190731.0

--- Shared/Scripts/BuildXLLkgVersionPublic.cmd
+++ Shared/Scripts/BuildXLLkgVersionPublic.cmd
@@ -1,3 +1,3 @@
 set BUILDXL_LKG_NAME=Microsoft.BuildXL.win-x64
-set BUILDXL_LKG_VERSION=0.1.0-20190730.6
+set BUILDXL_LKG_VERSION=0.1.0-20190731.0
 set BUILDXL_LKG_FEED_1=https://dotnet.myget.org/F/buildxl-selfhost/api/v3/index.json

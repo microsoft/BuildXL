8681d7d8cde5e1fe927d0ad7f130ea6acd1e4c4e
Rijul Luman <54083680+rijulluman-ms@users.noreply.github.com>
2019-08-15 13:38:16 -0700

    Invalid json validation Unit Test for bug 1579119 (#739)
    * Added a Unit Test to verify Quote Escape, bug 1579119
    
    * Using String interpolation
    
    * Removed unused imports

---
+++ Public/Src/Tools/UnitTests/Analyzers/PipExecutionPerformanceAnalyzerTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using BuildXL.Execution.Analyzer;
+using Test.BuildXL.TestUtilities.Xunit;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Test.Tool.Analyzers
+{
+    public class PipExecutionPerformanceAnalyzerTests : AnalyzerTestBase
+    {
+        public PipExecutionPerformanceAnalyzerTests(ITestOutputHelper output) : base(output)
+        {
+           
+        }
+
+        [Fact]
+        public void QuoteEscapeValidation()
+        {
+            char quote = '"';
+            char slash = '\\';
+            XAssert.AreEqual($"{slash}{slash}", PipExecutionPerformanceAnalyzer.NormalizeString($"{slash}"));
+            XAssert.AreEqual($"{slash}{quote}", PipExecutionPerformanceAnalyzer.NormalizeString($"{quote}"));
+            XAssert.AreEqual($"{slash}{slash}{slash}{quote}", PipExecutionPerformanceAnalyzer.NormalizeString($"{slash}{quote}"));
+            XAssert.AreEqual($"{slash}{slash}{slash}{slash}{slash}{quote}", PipExecutionPerformanceAnalyzer.NormalizeString($"{slash}{slash}{quote}"));
+        }
+
+    }
+}

6115812c9a76d21b4f05086608969ba973fbaf5a
Juan Carlos Guzman <juguzman@microsoft.com>
2019-08-28 07:34:31 -0700

    Add CounterTracker

--- Public/Src/Cache/ContentStore/Library/Tracing/CounterUtilities.cs
+++ Public/Src/Cache/ContentStore/Library/Tracing/CounterUtilities.cs
@@ -1,6 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
 using BuildXL.Cache.ContentStore.UtilitiesCore;
 using BuildXL.Utilities;
 using BuildXL.Utilities.Tracing;
@@ -32,6 +36,23 @@ namespace BuildXL.Cache.ContentStore.Tracing
             }
 
             return counterSet;
+        }
+
+        private static readonly ConcurrentDictionary<Type, Func<CounterCollection, CounterSet>> CachedRelfectionMethods = new ConcurrentDictionary<Type, Func<CounterCollection, CounterSet>>();
+        private static MethodInfo CachedGenericMethodDefiinition = null;
+
+        /// <nodoc />
+        public static CounterSet ToCounterSet(this CounterCollection counters, Type enumType)
+        {
+            // We cache the method since using Reflection is an expensive operation.
+            var method = CachedRelfectionMethods.GetOrAdd(enumType, theEnumType =>
+            {
+                CachedGenericMethodDefiinition = CachedGenericMethodDefiinition ?? typeof(CounterUtilities).GetMethods().Single(method => method.Name == nameof(ToCounterSet) && method.IsGenericMethodDefinition);
+                var genericMethod = CachedGenericMethodDefiinition.MakeGenericMethod(theEnumType);
+                return counterCollection => (CounterSet)genericMethod.Invoke(null, new object[] { counterCollection });
+            });
+
+            return method(counters);
         }
     }
 }

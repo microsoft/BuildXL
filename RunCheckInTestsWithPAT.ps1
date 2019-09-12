ae82a5db6d1e25c9880e41cb325c3f5c866fb8ce
Edgar Ruiz Silva <edgarsil@microsoft.com>
2019-09-12 13:41:34 -0700

    Add more params

--- RunCheckInTestsWithPAT.ps1
+++ RunCheckInTestsWithPAT.ps1
@@ -4,7 +4,12 @@
  [Parameter(mandatory=$true)]
  [String]$value,
  [Parameter(mandatory=$true)]
+ [String]$name2,
+ [Parameter(mandatory=$true)]
+ [String]$value2,
+ [Parameter(mandatory=$true)]
  [String]$arg
 )
 [Environment]::SetEnvironmentVariable($name, $value,"Process")
+[Environment]::SetEnvironmentVariable($name2, $value2,"Process")
 .\RunCheckInTests.cmd /lab $arg /internal

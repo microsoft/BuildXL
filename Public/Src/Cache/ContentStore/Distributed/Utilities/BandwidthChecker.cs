4a717df1d2d4a73775b55e0efdf9bba4c0c66bd4
Juan Carlos Guzman <juguzman@microsoft.com>
2019-09-16 12:53:05 -0700

    Add BandwidthChecker

---
+++ Public/Src/Cache/ContentStore/Distributed/Utilities/BandwidthChecker.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using BuildXL.Cache.ContentStore.Interfaces.Extensions;
+using BuildXL.Cache.ContentStore.Tracing.Internal;
+
+namespace BuildXL.Cache.ContentStore.Distributed.Utilities
+{
+    /// <summary>
+    /// Checks that a copy has a minimum bandwidth, and cancells copies otherwise.
+    /// </summary>
+    public class BandwidthChecker
+    {
+        private const double BytesInMb = 1024 * 1024;
+
+        private readonly HistoricalBandwidthLimitSource _historicalBandwidthLimitSource;
+        private readonly IBandwidthLimitSource _bandwidthLimitSource;
+        private readonly TimeSpan _checkInterval;
+
+        /// <nodoc />
+        public BandwidthChecker(IBandwidthLimitSource bandwidthLimitSource, TimeSpan checkInterval)
+        {
+            _bandwidthLimitSource = bandwidthLimitSource;
+            _checkInterval = checkInterval;
+            _historicalBandwidthLimitSource = _bandwidthLimitSource as HistoricalBandwidthLimitSource;
+        }
+
+        /// <summary>
+        /// Checks that a copy has a minimum bandwidth, and cancells it otherwise.
+        /// </summary>
+        /// <param name="context">The context of the operation.</param>
+        /// <param name="copyTaskFactory">Function that will trigger the copy.</param>
+        /// <param name="destinationStream">Stream into which the copy is being made. Used to meassure bandwidth.</param>
+        public async Task CheckBandwidthAtIntervalAsync(OperationContext context, Func<CancellationToken, Task> copyTaskFactory, Stream destinationStream)
+        {
+            if (_historicalBandwidthLimitSource != null)
+            {
+                var startPosition = destinationStream.Position;
+                var timer = Stopwatch.StartNew();
+                await Impl();
+                timer.Stop();
+                var endPosition = destinationStream.Position;
+
+                // Bandwidth checker expects speed in MiB/s, so convert it.
+                var bytesCopied = endPosition - startPosition;
+                var speed = bytesCopied / timer.Elapsed.TotalSeconds / (1024 * 1024);
+                await _historicalBandwidthLimitSource.AddBandwidthRecordAsync(speed, context.Token);
+            }
+            else
+            {
+                await Impl();
+            }
+
+            async Task Impl()
+            {
+                // This method should not fail with exceptions because the resulting task may be left unobserved causing an application to crash
+                // (given that the app is configured to fail on unobserved task exceptions).
+                var minimumSpeedInMbPerSec = await _bandwidthLimitSource.GetMinimumSpeedInMbPerSecAsync(context.Token);
+
+                long previousPosition = 0;
+                var copyCompleted = false;
+                using var copyCancellation = CancellationTokenSource.CreateLinkedTokenSource(context.Token);
+                var copyTask = copyTaskFactory(copyCancellation.Token);
+
+                try
+                {
+                    while (!copyCompleted)
+                    {
+                        // Wait some time for bytes to be copied
+                        var firstCompletedTask = await Task.WhenAny(copyTask,
+                            Task.Delay(_checkInterval, context.Token));
+
+                        copyCompleted = firstCompletedTask == copyTask;
+                        if (copyCompleted)
+                        {
+                            await copyTask;
+                            return;
+                        }
+                        else if (context.Token.IsCancellationRequested)
+                        {
+                            context.Token.ThrowIfCancellationRequested();
+                            return;
+                        }
+
+                        // Copy is not completed and operation has not been canceled, perform
+                        // bandwidth check 
+                        try
+                        {
+                            var position = destinationStream.Position;
+
+                            var receivedMiB = (position - previousPosition) / BytesInMb;
+                            var currentSpeed = receivedMiB / _checkInterval.TotalSeconds;
+                            if (currentSpeed < minimumSpeedInMbPerSec)
+                            {
+                                throw new TimeoutException($"Average speed was {currentSpeed}MiB/s - under {minimumSpeedInMbPerSec}MiB/s requirement. Aborting copy with {position} copied]");
+                            }
+
+                            previousPosition = position;
+                        }
+                        catch (ObjectDisposedException)
+                        {
+                            // If the check task races with the copy completing, it might attempt to check the position of a disposed stream.
+                            // Don't bother logging because the copy completed successfully.
+                        }
+                        catch (Exception ex)
+                        {
+                            var errorMessage = $"Exception thrown while checking bandwidth: {ex}";
+
+                            // Erring on the side of caution; if something went wrong with the copy, return to avoid spin-logging the same exception.
+                            // Converting TaskCanceledException to TimeoutException because the clients should know that the operation was cancelled due to timeout.
+                            throw new TimeoutException(errorMessage, ex);
+                        }
+                    }
+                }
+                finally
+                {
+                    if (!copyCompleted)
+                    {
+                        // Ensure that we signal the copy to cancel
+                        copyCancellation.Cancel();
+                        copyTask.FireAndForget(context);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Public/Src/Cache/ContentStore/Distributed/Utilities/IBandwidthLimitSource.cs
@@ -0,0 +1,143 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BuildXL.Cache.ContentStore.Distributed.Utilities
+{
+    /// <summary>
+    /// Gets the bandwidth lower limit for a bandwidth checker.
+    /// </summary>
+    public interface IBandwidthLimitSource
+    {
+        /// <nodoc />
+        Task<double> GetMinimumSpeedInMbPerSecAsync(CancellationToken token);
+    }
+
+    /// <nodoc />
+    public class ConstantBandwidthLimit : IBandwidthLimitSource
+    {
+        private readonly Task<double> _limitTask;
+
+        /// <nodoc />
+        public ConstantBandwidthLimit(double limit) => _limitTask = Task.FromResult(limit);
+
+        /// <inheritdoc />
+        public Task<double> GetMinimumSpeedInMbPerSecAsync(CancellationToken token) => _limitTask;
+    }
+
+    /// <summary>
+    /// Gets the bandwidth limit based on historical data of copies.
+    /// </summary>
+    public class HistoricalBandwidthLimitSource : IBandwidthLimitSource
+    {
+        private readonly int _maxRecordsStored;
+        private readonly Queue<double> _bandwidthRecords = new Queue<double>();
+        private readonly SemaphoreSlim _bandwidthRecordLock = new SemaphoreSlim(1, 1);
+
+        public HistoricalBandwidthLimitSource(int maxRecordsStored = 64) => _maxRecordsStored = maxRecordsStored;
+
+        /// <summary>
+        /// Adds a record for future use.
+        /// </summary>
+        public async Task AddBandwidthRecordAsync(double value, CancellationToken ct)
+        {
+            // Do a quick check to protect ourselves against invalid data: NaN's, negative, and zero values.
+            if (value <= 0.0)
+            {
+                return;
+            }
+
+            await _bandwidthRecordLock.WaitAsync(ct);
+            try
+            {
+                while (_bandwidthRecords.Count >= _maxRecordsStored)
+                {
+                    _bandwidthRecords.Dequeue();
+                }
+
+                _bandwidthRecords.Enqueue(value);
+            }
+            finally
+            {
+                _bandwidthRecordLock.Release();
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task<double> GetMinimumSpeedInMbPerSecAsync(CancellationToken token)
+        {
+            var statistics = await GetBandwidthRecordStatisticsAsync(token);
+            if (statistics.Count < _maxRecordsStored / 2)
+            {
+                // If not enough records, don't impose a limit.
+                return (0.0);
+            }
+            else
+            {
+                // Set the fiducial limit to 2.27 SD below the mean, or about the bottom 1% for normally distributed values.
+                // Use the minimum recorded speed as a lower bound to make sure we don't get negative or otherwise
+                // ridiculous limit values for unusual data patterns.
+                // Then allow copies to be up to twice that slow, so we don't time out even 1% of successful transfers.
+                var limit = 0.5 * Math.Max(statistics.Mean - 2.27 * statistics.StandardDeviation, statistics.Minimum);
+                return (limit);
+            }
+        }
+
+        private async Task<SummaryStatistics> GetBandwidthRecordStatisticsAsync(CancellationToken token)
+        {
+            var n = 0;
+            var mean = 0.0;
+            var standardDeviation = 0.0;
+            var min = double.MaxValue;
+            var max = double.MinValue;
+            await _bandwidthRecordLock.WaitAsync(token);
+            try
+            {
+                // This is the standard one-pass algorithm for summary statistic computation.
+                // See https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Online_algorithm for background.
+                foreach (var value in _bandwidthRecords)
+                {
+                    n++;
+                    var delta = value - mean;
+                    mean += delta / n;
+                    standardDeviation += delta * (value - mean);
+
+                    if (value < min)
+                    {
+                        min = value;
+                    }
+
+                    if (value > max)
+                    {
+                        max = value;
+                    }
+                }
+            }
+            finally
+            {
+                _bandwidthRecordLock.Release();
+            }
+
+            standardDeviation = Math.Sqrt(standardDeviation / (n - 1));
+
+            return new SummaryStatistics { Count = n, Mean = mean, StandardDeviation = standardDeviation, Minimum = min, Maximum = max };
+        }
+
+        private struct SummaryStatistics
+        {
+            public int Count;
+
+            public double Mean;
+
+            public double StandardDeviation;
+
+            public double Minimum;
+
+            public double Maximum;
+        }
+    }
+}

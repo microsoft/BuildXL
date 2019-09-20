711634160cf7da5f02f400757a937540e73d4fec
Rijul Luman <54083680+rijulluman-msft@users.noreply.github.com>
2019-09-20 10:34:50 -0700

    Component Governance for NuGet Packages (#889)
    * Skeleton code for generating cgmanifest.json for nugets
    
    * some unit tests
    
    * remove unused using
    
    * Sorted cgmanifest json generation complete
    
    * cgmanifest.json write partial changes
    
    * cgmanifest.json generation and validation complete
    
    * Added Validate cgmanifest steps
    
    * cgmanifest validation eroor messages updated
    
    * Added help text
    
    * Unit Test for cgmanifet complete
    
    * Code clean up
    
    * Merge fix
    
    * Added missing git package file for component governance
    
    * Suggested Comment fixes
    
    * Added error logs before all CG Manifest build failures
    
    * nit changes
    
    * Unit Test path changes for Mac

---
+++ Public/Src/FrontEnd/Nuget/NugetCgManifestGenerator.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Linq;
+using BuildXL.FrontEnd.Sdk;
+using BuildXL.Utilities.Collections;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BuildXL.FrontEnd.Nuget
+{
+    /// <summary>
+    /// NugetCgManifestGenerator is used for creation and comparasion of a manifest file for Component Governance.
+    /// The cgmanifest file contains information about all the Nuget Packages used in BuildXL with all their versions in use
+    /// The cgmanifest file is used by Component Governance to determine security risks within components used by BuildXL
+    /// This manifest file will only be picked up for Component Governance if it is named "cgmanifest.json" as per cg documentation: https://docs.opensource.microsoft.com/tools/cg.html
+    /// </summary>
+    public sealed class NugetCgManifestGenerator
+    {
+        private FrontEndContext Context { get; }
+
+        /// <nodoc />
+        public NugetCgManifestGenerator(FrontEndContext context)
+        {
+            Context = context;
+        }
+
+        /// <summary>
+        /// Generates json as an indented string containing all the NuGet package names and versions used in BuildXL
+        /// </summary>
+        public string GenerateCgManifestForPackages(MultiValueDictionary<string, Package> packages)
+        {
+            var components = packages
+                .Keys
+                .SelectMany(nugetName => packages[nugetName].Select(package => new NugetPackageAndVersionStore(nugetName, ExtractNugetVersion(package))))
+                .OrderBy(c => c.Name)
+                .ThenBy(c => c.Version)
+                .Select(c => ToNugetComponent(c.Name, c.Version))
+                .ToList();
+
+            var cgmanifest = new
+            {
+                Version = 1,
+                Registrations = components
+            };
+
+            return JsonConvert.SerializeObject(cgmanifest, Formatting.Indented);
+        }
+
+        /// <summary>
+        /// Compares <paramref name="lhsManifest"/> and <paramref name="rhsManifest"/> for equality;
+        /// returns true if they are equal, and false otherwise. 
+        /// 
+        /// This equality check is case-insensitive and white space agnostic.
+        /// </summary>
+        public bool CompareForEquality(string lhsManifest, string rhsManifest)
+        {
+            try
+            {
+                return JToken.DeepEquals(JObject.Parse(lhsManifest), JObject.Parse(rhsManifest));
+            }
+            catch (JsonReaderException)
+            {
+                // The existing Manifest file was in invalid JSON format.
+                // Hence it does not match.
+                return false;
+            }
+            
+        }
+
+        private string ExtractNugetVersion(Package p)
+        {
+            return p.Path.GetParent(Context.PathTable).GetName(Context.PathTable).ToString(Context.StringTable);
+        }
+
+        private object ToNugetComponent(string name, string version)
+        {
+            return new
+            {
+                Component = new
+                {
+                    Type = "NuGet",
+                    NuGet = new
+                    {
+                        Name = name,
+                        Version = version
+                    }
+                }
+            };
+        }
+
+        private class NugetPackageAndVersionStore {
+            public string Name { get; }
+            public string Version { get; }
+
+            public NugetPackageAndVersionStore(string name, string version) {
+                Name = name;
+                Version = version;
+            }
+        }
+    }
+}

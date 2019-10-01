6d1ab932af66b591a107eebba84e7f4e2319763b
Sergey Teplyakov <Sergey.Teplyakov@gmail.com>
2019-10-01 14:42:35 -0700

    More comments and rename the option to disable reconciliation (#972)
    * More comments and rename the option to disable reconciliation
    
    * Address CR comments.

--- Public/Src/Cache/DistributedCache.Host/Service/Internal/DistributedContentStoreFactory.cs
+++ Public/Src/Cache/DistributedCache.Host/Service/Internal/DistributedContentStoreFactory.cs
@@ -301,7 +301,9 @@ namespace BuildXL.Cache.Host.Service.Internal
             ApplyIfNotNull(
                 _distributedSettings.SafeToLazilyUpdateMachineCountThreshold,
                 value => configuration.SafeToLazilyUpdateMachineCountThreshold = value);
-            ApplyIfNotNull(_distributedSettings.IsReconciliationEnabled, value => configuration.EnableReconciliation = value);
+
+            configuration.EnableReconciliation = !_distributedSettings.Unsafe_DisableReconciliation;
+
             ApplyIfNotNull(_distributedSettings.UseIncrementalCheckpointing, value => configuration.Checkpoint.UseIncrementalCheckpointing = value);
             ApplyIfNotNull(_distributedSettings.IncrementalCheckpointDegreeOfParallelism, value => configuration.Checkpoint.IncrementalCheckpointDegreeOfParallelism = value);
 

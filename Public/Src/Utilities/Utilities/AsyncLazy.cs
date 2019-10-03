5be1d3717e07beac9a6999a79e66ff8ee6762053
Lance Collins <lancec@microsoft.com>
2019-10-02 17:16:50 -0700

    Improve time to first pip on workers by having workers download graph while scheduler is initializing/filtering. Also improves, time to first pip on master by making historic metadata and historic perf data retrieval lazy and async. For Office Mularchy builds, this results in workers being ready to execute about the same time as the master.  Specifically for master (13.5 min to 11 min) and for worker (17 min to 11 min).

---
+++ Public/Src/Utilities/Utilities/AsyncLazy.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading.Tasks;
+using BuildXL.Utilities.Tasks;
+
+namespace BuildXL.Utilities
+{
+    /// <summary>
+    /// Represents an async version of <see cref="Lazy{T}"/> which allows starting the underlying task to without waiting on the result
+    /// </summary>
+    public sealed class AsyncLazy<T>
+    {
+        private Lazy<Task<T>> m_lazyTask;
+
+        /// <nodoc />
+        public AsyncLazy(Func<Task<T>> factory)
+        {
+            m_lazyTask = new Lazy<Task<T>>(() => Task.Run(factory));    
+        }
+
+        /// <summary>
+        /// Gets the synchronous result of the completion of the async lazy
+        /// </summary>
+        public T Value => m_lazyTask.Value.GetAwaiter().GetResult();
+
+        /// <summary>
+        /// Starts the asynchronous operation without blocking the current thread of execution
+        /// </summary>
+        public void Start()
+        {
+            m_lazyTask.Value.Forget();
+        }
+    }
+}

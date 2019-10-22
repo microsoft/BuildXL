6e44721e59d39ce2d07089dfdfd453d7d90f6365
Kristijan Šimić <1250428+Kr1sso@users.noreply.github.com>
2019-10-22 12:03:13 +0200

    Add some more assertions for sanity checking (#1066)

--- Public/Src/Engine/Scheduler/Scheduler.cs
+++ Public/Src/Engine/Scheduler/Scheduler.cs
@@ -4058,6 +4058,10 @@ namespace BuildXL.Scheduler
 
         private async Task<PipResult> ExecuteNonProcessPipAsync(RunnablePip runnablePip)
         {
+            Contract.Requires(runnablePip.Pip != null);
+            Contract.Requires(runnablePip.OperationContext.IsValid);
+            Contract.Requires(runnablePip.Environment != null);
+
             var pip = runnablePip.Pip;
             var operationContext = runnablePip.OperationContext;
             var environment = runnablePip.Environment;

aaeb8c49b926bc74bc85cc5ae2fd8d27d2ac968d
Rijul Luman <rijul.luman@microsoft.com>
2019-10-28 15:09:43 -0700

    Revert test LKG

--- Shared/Scripts/BuildXLLkgVersion.cmd
+++ Shared/Scripts/BuildXLLkgVersion.cmd
@@ -1,5 +1,3 @@
 set BUILDXL_LKG_NAME=BuildXL.win-x64
-REM set BUILDXL_LKG_VERSION=0.1.0-20191021.0
-REM set BUILDXL_LKG_FEED_1=https://pkgs.dev.azure.com/1essharedassets/_packaging/BuildXL/nuget/v3/index.json
-set BUILDXL_LKG_VERSION=0.0.44897-pr.public.win
-set BUILDXL_LKG_FEED_1=F:\temp
+set BUILDXL_LKG_VERSION=0.1.0-20191021.0
+set BUILDXL_LKG_FEED_1=https://pkgs.dev.azure.com/1essharedassets/_packaging/BuildXL/nuget/v3/index.json

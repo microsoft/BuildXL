14913523c64ba2f0d9c266d0eea0d9e2035acefe
Rijul Luman <rijul.luman@microsoft.com>
2019-10-28 14:02:44 -0700

    Manual LKG trial

--- Shared/Scripts/BuildXLLkgVersion.cmd
+++ Shared/Scripts/BuildXLLkgVersion.cmd
@@ -1,3 +1,4 @@
 set BUILDXL_LKG_NAME=BuildXL.win-x64
 set BUILDXL_LKG_VERSION=0.1.0-20191021.0
-set BUILDXL_LKG_FEED_1=https://pkgs.dev.azure.com/1essharedassets/_packaging/BuildXL/nuget/v3/index.json
+REM set BUILDXL_LKG_FEED_1=https://pkgs.dev.azure.com/1essharedassets/_packaging/BuildXL/nuget/v3/index.json
+set BUILDXL_LKG_FEED_1=https://artprodwus21.artifacts.visualstudio.com/Aea964927-e753-4550-bf48-b63004033439/94af654b-2880-46f2-aed5-ab548dab8df0/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL21zL3Byb2plY3RJZC85NGFmNjU0Yi0yODgwLTQ2ZjItYWVkNS1hYjU0OGRhYjhkZjAvYnVpbGRJZC80NDg5Ny9hcnRpZmFjdE5hbWUvTWljcm9zb2Z0LkJ1aWxkWEwud2luLXg2NC5wa2c1/content?format=zip

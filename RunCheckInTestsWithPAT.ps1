b90ea87a794142104d33b8e2ee3f12d66206b104
Rijul Luman <rijul.luman@microsoft.com>
2019-10-28 10:47:47 -0700

    env variable test

--- RunCheckInTestsWithPAT.ps1
+++ RunCheckInTestsWithPAT.ps1
@@ -10,5 +10,5 @@
 )
 [Environment]::SetEnvironmentVariable("1ESSHAREDASSETS_BUILDXL_FEED_PAT", $1esPat, "Process")
 [Environment]::SetEnvironmentVariable("CLOUDBUILD_BUILDXL_SELFHOST_FEED_PAT", $cbPat, "Process")
-[Environment]::SetEnvironmentVariable("NUGET_CREDENTIALPROVIDERS_PATH", $ncPath)
+[Environment]::SetEnvironmentVariable("NUGET_CREDENTIALPROVIDERS_PATH", $ncPath, "Process")
 .\RunCheckInTests.cmd /lab $args /internal

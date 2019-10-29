7404ddfe81d9c6c7cdee911ea72a954ce5c9f07a
Sergey Tepliakov <Sergey.Teplyakov@gmail.com>
2019-10-29 10:01:45 -0700

    Exclude non-nullable attributes for .NET Core.

--- Public/Src/Cache/ContentStore/Interfaces/NonNullAttributes.cs
+++ Public/Src/Cache/ContentStore/Interfaces/NonNullAttributes.cs
@@ -1,33 +1,34 @@
-﻿#if !NET_CORE
+﻿// The following attributes already defined in System.Runtime for .NET Core APP
+#if !NET_COREAPP
 namespace System.Diagnostics.CodeAnalysis
 {
     /// <summary>Specifies that null is allowed as an input even if the corresponding type disallows it.</summary>
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
-    internal sealed class AllowNullAttribute : Attribute
+    [System.AttributeUsage(System.AttributeTargets.Field | System.AttributeTargets.Parameter | System.AttributeTargets.Property, Inherited = false)]
+    internal sealed class AllowNullAttribute : System.Attribute
     {
     }
 
     /// <summary>Specifies that null is disallowed as an input even if the corresponding type allows it.</summary>
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
-    internal sealed class DisallowNullAttribute : Attribute
+    [System.AttributeUsage(System.AttributeTargets.Field | System.AttributeTargets.Parameter | System.AttributeTargets.Property, Inherited = false)]
+    internal sealed class DisallowNullAttribute : System.Attribute
     {
     }
 
     /// <summary>Specifies that an output may be null even if the corresponding type disallows it.</summary>
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
-    internal sealed class MaybeNullAttribute : Attribute
+    [System.AttributeUsage(System.AttributeTargets.Field | System.AttributeTargets.Parameter | System.AttributeTargets.Property | System.AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed class MaybeNullAttribute : System.Attribute
     {
     }
 
     /// <summary>Specifies that an output will not be null even if the corresponding type allows it.</summary>
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
-    internal sealed class NotNullAttribute : Attribute
+    [System.AttributeUsage(System.AttributeTargets.Field | System.AttributeTargets.Parameter | System.AttributeTargets.Property | System.AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed class NotNullAttribute : System.Attribute
     {
     }
 
     /// <summary>Specifies that when a method returns <see cref="ReturnValue"/>, the parameter may be null even if the corresponding type disallows it.</summary>
-    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    internal sealed class MaybeNullWhenAttribute : Attribute
+    [System.AttributeUsage(System.AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class MaybeNullWhenAttribute : System.Attribute
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="MaybeNullWhenAttribute"/> class.
@@ -42,8 +43,8 @@ namespace System.Diagnostics.CodeAnalysis
     }
 
     /// <summary>Specifies that when a method returns <see cref="ReturnValue"/>, the parameter will not be null even if the corresponding type allows it.</summary>
-    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    internal sealed class NotNullWhenAttribute : Attribute
+    [System.AttributeUsage(System.AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class NotNullWhenAttribute : System.Attribute
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="NotNullWhenAttribute"/> class.
@@ -58,8 +59,8 @@ namespace System.Diagnostics.CodeAnalysis
     }
 
     /// <summary>Specifies that the output will be non-null if the named parameter is non-null.</summary>
-    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
-    internal sealed class NotNullIfNotNullAttribute : Attribute
+    [System.AttributeUsage(System.AttributeTargets.Parameter | System.AttributeTargets.Property | System.AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
+    internal sealed class NotNullIfNotNullAttribute : System.Attribute
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="NotNullIfNotNullAttribute"/> class.
@@ -74,14 +75,14 @@ namespace System.Diagnostics.CodeAnalysis
     }
 
     /// <summary>Applied to a method that will never return under any circumstance.</summary>
-    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
-    internal sealed class DoesNotReturnAttribute : Attribute
+    [System.AttributeUsage(System.AttributeTargets.Method, Inherited = false)]
+    internal sealed class DoesNotReturnAttribute : System.Attribute
     {
     }
 
     /// <summary>Specifies that the method will not return if the associated Boolean parameter is passed the specified value.</summary>
-    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-    internal sealed class DoesNotReturnIfAttribute : Attribute
+    [System.AttributeUsage(System.AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class DoesNotReturnIfAttribute : System.Attribute
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="DoesNotReturnIfAttribute"/> class.

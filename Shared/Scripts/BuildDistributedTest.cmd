4910be5f801b33298590650497c8341a938a5e64
Rijul Luman <rijul.luman@microsoft.com>
2019-10-29 12:14:51 -0700

    Revert env changes

--- Shared/Scripts/BuildDistributedTest.cmd
+++ Shared/Scripts/BuildDistributedTest.cmd
@@ -50,14 +50,13 @@ if DEFINED MSENG_GIT_PAT (
     set DistributedBuildTestURL=https://%MSENG_GIT_PAT%@mseng.visualstudio.com/Domino/_git/Domino.DistributedBuildTest
 )
 
+echo MSENG_GIT_PAT: %MSENG_GIT_PAT%
+echo DistributedBuildTestURL:  %DistributedBuildTestURL% 
 set DistributedBuildTestURL
-echo DistributedBuildTestURL per:  %DistributedBuildTestURL% 
-echo DistributedBuildTestURL:  DistributedBuildTestURL
-echo DistributedBuildTestURL dol:  $DistributedBuildTestURL
 echo %DistributedBuildTestURL% %TEST_SOLUTION_ROOT%
-echo git clone DistributedBuildTestURL %TEST_SOLUTION_ROOT% 2>&1
+echo git clone %DistributedBuildTestURL% %TEST_SOLUTION_ROOT% 2>&1
 
-call "%ProgramFiles%\Git\cmd\git" clone DistributedBuildTestURL %TEST_SOLUTION_ROOT% 2>&1
+call "%ProgramFiles%\Git\cmd\git" clone %DistributedBuildTestURL% %TEST_SOLUTION_ROOT% 2>&1
 if %ERRORLEVEL% NEQ 0 (
     endlocal && exit /b 1
 )

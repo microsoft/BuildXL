6b7f81f1aae690ba8eb7e6aafece72b49ccef223
CloudDev Build Account <tsebuild@microsoft.com>
2019-11-02 05:00:36 +0000

    New LKG 0.1.0-20191101.6

--- Shared/Scripts/BuildXLLkgVersionPublic.cmd
+++ Shared/Scripts/BuildXLLkgVersionPublic.cmd
@@ -1,3 +1,3 @@
 set BUILDXL_LKG_NAME=Microsoft.BuildXL.win-x64
-set BUILDXL_LKG_VERSION=0.1.0-20191031.5
+set BUILDXL_LKG_VERSION=0.1.0-20191101.6
 set BUILDXL_LKG_FEED_1=https://pkgs.dev.azure.com/ms/BuildXL/_packaging/BuildXL/nuget/v3/index.json

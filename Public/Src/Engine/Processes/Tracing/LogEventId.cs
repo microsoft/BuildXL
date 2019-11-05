ba1146dab01c704159c03a7c157f0541bf7024b7
Semih Okur <seokur@microsoft.com>
2019-11-04 21:48:59 -0800

    Remove contract assertion

--- Public/Src/Engine/Processes/Tracing/LogEventId.cs
+++ Public/Src/Engine/Processes/Tracing/LogEventId.cs
@@ -131,7 +131,6 @@ namespace BuildXL.Processes.Tracing
         FailedToCreateHardlinkOnMerge = 12209,
         DoubleWriteAllowedDueToPolicy = 12210,
         DisallowedDoubleWriteOnMerge = 12211,
-        MoreBytesWrittenThanBufferSize = 12212,
 
         //// Special tool errors
         PipProcessToolErrorDueToHandleToFileBeingUsed = 14300,

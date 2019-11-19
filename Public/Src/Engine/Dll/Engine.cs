0d8ed411227be31ef42ac95933c83ac3f1d310d2
Semih Okur <seokur@microsoft.com>
2019-11-19 10:48:18 -0800

    Enable earlyWorkerRelease by default

--- Public/Src/Engine/Dll/Engine.cs
+++ Public/Src/Engine/Dll/Engine.cs
@@ -1154,6 +1154,12 @@ namespace BuildXL.Engine
                 mutableConfig.Distribution.EarlyWorkerRelease = false;
             }
 
+            if (mutableConfig.Logging.Environment == ExecutionEnvironment.OfficeAPEXLab)
+            {
+                // Disable EarlyWorkerRelease for APEX builds for now.
+                mutableConfig.Distribution.EarlyWorkerRelease = false;
+            }
+
             // When running in cloudbuild we want to ignore the user setting the interactive flag
             // and force it to be false since we never want to pop up UI there.
             if (mutableConfig.InCloudBuild())

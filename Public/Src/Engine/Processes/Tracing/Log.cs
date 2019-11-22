c2232d2d39ed356a9307b16ccb766bf40a8a59e6
Aleksandar Milicevic <almili@microsoft.com>
2019-11-21 16:33:14 -0800

    Fix DetoursDebugMessage log message

--- Public/Src/Engine/Processes/Tracing/Log.cs
+++ Public/Src/Engine/Processes/Tracing/Log.cs
@@ -381,7 +381,7 @@ namespace BuildXL.Processes.Tracing
             EventLevel = Level.Verbose,
             Keywords = (int)Keywords.UserMessage,
             EventTask = (int)Tasks.PipExecutor,
-            Message = "[{0:X16}] Detours Debug Message: {message}")]
+            Message = "[Pip{pipSemiStableHash:X16}] Detours Debug Message: {message}")]
         public abstract void LogDetoursDebugMessage(
             LoggingContext context,
             long pipSemiStableHash,

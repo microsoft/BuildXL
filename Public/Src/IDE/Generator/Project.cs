1d19b19f4f64ff7033546aff7834e4a703532dae
Lance Collins <lancec@microsoft.com>
2019-12-19 17:09:40 +0000

    Merged PR 525089: Specify OutputPath/OutDir to allow discovering test inside VS. Common.targets
    Specify OutputPath/OutDir to allow discovering test inside VS. Common.targets is not imported in-place of Directory.Build.targets so that it is late enough to override Build.

--- Public/Src/IDE/Generator/Project.cs
+++ Public/Src/IDE/Generator/Project.cs
@@ -53,12 +53,8 @@ namespace BuildXL.Ide.Generator
             {
                 m_outputDirectoryType = outputDirectoryType;
                 SetProperty("DominoBuildFilter", buildFilter);
-                
-                // Intentionally not setting either "OutputPath" or "OutDir" because 'outputDirectory' 
-                // is a BuildXL folder and so if VS tries to write to it access will be denied.
-                //
-                // Instead, output folders are set in Directory.Build.props, which is automatically
-                // generated and placed in the root of the enlistment.
+                SetProperty("OutputPath", outputDirectory);
+                SetProperty("OutDir", outputDirectory);
             }
         }
 

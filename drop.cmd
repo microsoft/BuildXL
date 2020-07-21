6e955fdf5d334bc990943a5e4750d2cde5f44e77
Kristijan Simic <krsimic@microsoft.com>
2020-07-21 21:16:01 +0000

    Merged PR 565168: Update drop.cmd bootstrap script
    Update drop.cmd script to bootstrap Drop.App, Artifact services had a TLS update and the old version is no longer working, see:
    
    https://portal.microsofticm.com/imp/v3/incidents/details/196705598/home

--- drop.cmd
+++ drop.cmd
@@ -1,9 +1,7 @@
 @echo off
 setlocal
 
-set DropVersion=0.100.0-rc3146171
-
-set DEFAULT_DROP_EXE_LOCATION=%~dp0Out\SelfHost\Drop\Drop.App.%DropVersion%\lib\net45
+set DEFAULT_DROP_EXE_LOCATION=%~dp0Out\SelfHost\Drop.App\lib\net45
 
 if EXIST %DEFAULT_DROP_EXE_LOCATION%\drop.exe (
 	set DROP_EXE_LOCATION=%DEFAULT_DROP_EXE_LOCATION%
@@ -12,9 +10,17 @@ if EXIST %DEFAULT_DROP_EXE_LOCATION%\drop.exe (
 if NOT DEFINED DROP_EXE_LOCATION (
 	REM Potentiall init.cmd has set the credential provider path to a mapped b-drive. Temporarilly undo this.
 	set OLD_NUGET_CREDENTIALPROVIDERS_PATH=%NUGET_CREDENTIALPROVIDERS_PATH%
-    set NUGET_CREDENTIALPROVIDERS_PATH=
+	set NUGET_CREDENTIALPROVIDERS_PATH=
 
-	%~dp0Shared\Tools\nuget.exe install -OutputDirectory %~dp0Out\SelfHost\Drop -Source "https://pkgs.dev.azure.com/cloudbuild/_packaging/BuildXL.Selfhost/nuget/v3/index.json" Drop.App -Version %DropVersion%
+	REM Delete any leftovers, e.g. the exe is missing but other files are still present
+	rmdir /Q /S %~dp0Out\SelfHost\Drop.App
+
+	REM Create the drop app folder, download the latest version and unzip it
+	mkdir %~dp0Out\SelfHost\Drop.App
+	curl "https://artifacts.dev.azure.com/cloudbuild/_apis/drop/client/exe" --output %~dp0Out\SelfHost\Drop.App.zip
+	tar -xf %~dp0Out\SelfHost\Drop.App.zip -C %~dp0Out\SelfHost\Drop.App
+	del %~dp0Out\SelfHost\Drop.App.zip
+
 	set DROP_EXE_LOCATION=%DEFAULT_DROP_EXE_LOCATION%
 	
 	REM Restore credential provider path.

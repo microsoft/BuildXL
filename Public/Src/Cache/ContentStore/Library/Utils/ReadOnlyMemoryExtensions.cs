47ac25df6a58269da21243943180af6001554ce0
Sergey Tepliakov <seteplia@microsoft.com>
2021-02-12 01:34:30 +0000

    Merged PR 597680: Add ReadOnlyMemoryExtensions for getting MemoryStream from ReadOnlyMemory
    Add ReadOnlyMemoryExtensions for getting MemoryStream from ReadOnlyMemory

---
+++ Public/Src/Cache/ContentStore/Library/Utils/ReadOnlyMemoryExtensions.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace BuildXL.Cache.ContentStore.Utils
+{
+    // TODO: this class should be moved to utilities eventually, but right now we don't have an utilities project that references System.Memory
+    /// <summary>
+    /// Extension methods for <see cref="ReadOnlyMemory{T}"/>.
+    /// </summary>
+    public static class ReadOnlyMemoryExtensions
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="MemoryStream"/> and the flag that indicates whether a new array was allocated.
+        /// </summary>
+        /// <remarks>
+        /// The method tries to reuse an array the <paramref name="input"/> uses, but in some cases (theoretically)
+        /// the underlying array maybe unavailable and the new array will be created.
+        /// Btw, I haven't find a way to force a new array allocation here, but it is good to have a way to check it by having a flag in the result.
+        /// </remarks>
+        public static MemoryStream AsMemoryStream(this ReadOnlyMemory<byte> input, out bool newArrayWasCreated)
+        {
+            if (MemoryMarshal.TryGetArray(input, out var segment))
+            {
+                newArrayWasCreated = false;
+                return 
+                    new MemoryStream(segment.Array!, index: segment.Offset, count: segment.Count, writable: false);
+            }
+
+            newArrayWasCreated = true;
+            return new MemoryStream(input.ToArray());
+        }
+
+        /// <inheritdoc cref="AsMemoryStream(System.ReadOnlyMemory{byte},out bool)"/>
+        public static MemoryStream AsMemoryStream(this ReadOnlyMemory<byte> input) => input.AsMemoryStream(out _);
+    }
+}
---
+++ Public/Src/Cache/ContentStore/Test/Utils/ReadOnlyMemoryExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using BuildXL.Cache.ContentStore.Utils;
+using FluentAssertions;
+using Xunit;
+
+namespace BuildXL.Cache.ContentStore.Test.Utils
+{
+    public class ReadOnlyMemoryExtensionsTests
+    {
+        [Fact]
+        public void ArrayIsReused()
+        {
+            var array = new byte[1] { 42 };
+            var memory = new ReadOnlyMemory<byte>(array);
+
+            using var memoryStream = memory.AsMemoryStream(out var newArrayWasCreated);
+            newArrayWasCreated.Should().BeFalse();
+            memoryStream.ToArray().Should().BeEquivalentTo(array);
+        }
+
+        [Fact]
+        public void SubArrayIsReused()
+        {
+            var array = new byte[] { 42, 1 };
+            var memory = new ReadOnlyMemory<byte>(array, 1, 1);
+
+            using var memoryStream = memory.AsMemoryStream(out var newArrayWasCreated);
+            newArrayWasCreated.Should().BeFalse();
+            memoryStream.ToArray().Should().BeEquivalentTo(memory.ToArray());
+        }
+
+        [Fact]
+        public void NewArrayIsReusedForEmptyMemory()
+        {
+            // This case can be optimized, but for now AsMemoryStream
+            // should return a new array if the empty (default) instance is passed.
+            var memory = ReadOnlyMemory<byte>.Empty;
+
+            using var memoryStream = memory.AsMemoryStream(out var newArrayWasCreated);
+            newArrayWasCreated.Should().BeFalse();
+            var length = memoryStream.ToArray().Length;
+            length.Should().Be(0);
+        }
+    }
+}

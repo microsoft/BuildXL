adc8f24a0c8fc8943b3ca226833da037541ec7c3
Sergey Tepliakov <seteplia@microsoft.com>
2021-04-06 23:46:00 +0000

    Merged PR 601578: GetOrAdd should return a newly created value on the first call
    The original version was losing the value that was added during the first call.
    
    (This method can be used win .netstandard2.0 and not only with .net451)

--- Public/Src/Cache/ContentStore/Library/Utils/ConcurrentDictionaryExtensions.cs
+++ Public/Src/Cache/ContentStore/Library/Utils/ConcurrentDictionaryExtensions.cs
@@ -19,12 +19,21 @@ namespace BuildXL.Cache.ContentStore.Utils
         [return: MaybeNull]
         public static TValue GetOrAdd<TKey, TValue, TArg>(this ConcurrentDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TArg, TValue> valueFactory, TArg factoryArgument) where TKey : notnull
         {
-            if (!dictionary.TryGetValue(key, out var value))
+            if (dictionary.TryGetValue(key, out var value))
             {
-                dictionary.TryAdd(key, valueFactory(key, factoryArgument));
+                return value;
             }
 
-            return value;
+            bool added = dictionary.TryAdd(key, value = valueFactory(key, factoryArgument));
+            if (added)
+            {
+                // We added the value, so the 'value' is what was added.
+                return value;
+            }
+
+            // We lost the race and someone already added the value.
+            // Calling the same method recursively to get the value or to create a new one if the value will be very quickly removed.
+            return dictionary.GetOrAdd<TKey, TValue, TArg>(key, valueFactory, factoryArgument);
         }
     }
 }
---
+++ Public/Src/Cache/ContentStore/Test/Utils/ConcurrentDictionaryExtensionsTests.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using BuildXL.Cache.ContentStore.InterfacesTest;
+using BuildXL.Cache.ContentStore.Utils;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ContentStoreTest.Utils
+{
+    public class ConcurrentDictionaryExtensionsTests : TestWithOutput
+    {
+        public ConcurrentDictionaryExtensionsTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact]
+        public void AddOrGetReturnsFirstResult()
+        {
+            var cd = new ConcurrentDictionary<int, int>();
+            var result = ConcurrentDictionaryExtensions.GetOrAdd(cd, 42,
+                (key, args) =>
+                {
+                    return 42;
+                }, factoryArgument: string.Empty);
+            result.Should().Be(42);
+        }
+
+        [Fact]
+        public async Task CallbackIsCalled()
+        {
+            int attemptCount = 100;
+
+            for (int i = 0; i < attemptCount; i++)
+            {
+                await checkConcurrentGetOrAdd();
+            }
+            
+            async Task checkConcurrentGetOrAdd()
+            {
+                var cd = new ConcurrentDictionary<int, int>();
+                const int key = 42;
+                bool factoryIsCalled = false;
+                Task<bool> addTask = Task.Run(() => cd.TryAdd(key, 1));
+                Task<int> addOrUpdateTask = Task.Run(
+                    () =>
+                    {
+                        return ConcurrentDictionaryExtensions.GetOrAdd(
+                            cd,
+                            key,
+                            (k, state) =>
+                            {
+                                factoryIsCalled = true;
+                                return 2;
+                            },
+                            factoryArgument: string.Empty);
+                    });
+
+                await Task.WhenAll(addTask, addOrUpdateTask);
+
+                var addOrUpdateResult = await addOrUpdateTask;
+
+                if (factoryIsCalled)
+                {
+                    // If factory was called, the result can still be obtained from the TryAdd call
+                    addOrUpdateResult.Should().BeInRange(1, 2);
+                }
+                else
+                {
+                    addOrUpdateResult.Should().Be(1);
+                }
+
+                // Printing the outcome just for debugging purposes.
+                if (factoryIsCalled && addOrUpdateResult == 1)
+                {
+                    Output.WriteLine("Factory was called, but the race was lost.");
+                }
+
+                if (factoryIsCalled && addOrUpdateResult == 2)
+                {
+                    Output.WriteLine("Factory was called, and the race was won.");
+                }
+
+                if (!factoryIsCalled)
+                {
+                    Output.WriteLine("Factory was not called");
+                }    
+            }    
+        }
+    }
+}

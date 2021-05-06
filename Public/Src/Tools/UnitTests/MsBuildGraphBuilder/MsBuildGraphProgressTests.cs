f78909fb184d712edc47e2035889722c36fe180f
Serge Mera <smera@microsoft.com>
2021-05-06 18:51:59 +0000

    Merged PR 611879: Double the pipe connection timeout for MSBuild progress reporting tests
    Double the pipe connection timeout for MSBuild progress reporting tests
    
    Related work items: #1840555

--- Public/Src/Tools/UnitTests/MsBuildGraphBuilder/MsBuildGraphProgressTests.cs
+++ Public/Src/Tools/UnitTests/MsBuildGraphBuilder/MsBuildGraphProgressTests.cs
@@ -101,7 +101,7 @@ namespace Test.ProjectGraphBuilder
                             PipeOptions.Asynchronous))
                         using (var reader = new StreamReader(pipeClient, Encoding.UTF8))
                         {
-                            pipeClient.Connect(50000);
+                            pipeClient.Connect(100000);
                             while (!reader.EndOfStream)
                             {
                                 var line = reader.ReadLine();

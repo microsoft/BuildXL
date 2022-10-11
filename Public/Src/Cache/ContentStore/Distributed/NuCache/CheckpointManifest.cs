440be6ec27568930ad9a23d6a4176762c27403b3
Julian Bayardo <jubayard@microsoft.com>
2022-10-11 23:27:40 +0000

    Merged PR 683281: Checkpoint manifest fix

--- Public/Src/Cache/ContentStore/Distributed/NuCache/CheckpointManifest.cs
+++ Public/Src/Cache/ContentStore/Distributed/NuCache/CheckpointManifest.cs
@@ -150,9 +150,13 @@ namespace BuildXL.Cache.ContentStore.Distributed.NuCache
             return JsonUtilities.JsonDeserialize<CheckpointManifest>(serialized);
         }
 
+        // TODO(jubayard, 2022/10/11): this is a temporary workaround to prevent old worker code from failing to
+        // deserialize newly produced checkpoint manifests.
+        private const string JsonManifestHeader = "// {} \n";
+
         public string ToJson()
         {
-            return JsonUtilities.JsonSerialize(this, indent: true);
+            return JsonManifestHeader + JsonUtilities.JsonSerialize(this, indent: true);
         }
     }
 }

b1e6aa25941b3e78a6915d585559185ec9dab900
Sergey Tepliakov <seteplia@microsoft.com>
2022-12-29 17:56:57 +0000

    Merged PR 695153: Add 'SetsRequiredMembersAttribute' and update 'CompilerFeatureRequierdAttribute'
    Add 'SetsRequiredMembersAttribute' and update 'CompilerFeatureRequierdAttribute'

--- Public/Sdk/SelfHost/BuildXL/RequiredAttribute.cs
+++ Public/Sdk/SelfHost/BuildXL/RequiredAttribute.cs
@@ -1,15 +1,21 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-// Copied from https://github/dotnet/runtime
-
 #if !NET7_0_OR_GREATER
+
+// Copied from:
+// https://github.com/dotnet/runtime/blob/fdd104ec5e1d0d2aa24a6723995a98d0124f724b/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/RequiredMemberAttribute.cs
 
 namespace System.Runtime.CompilerServices;
 
 /// <summary>Specifies that a type has required members or that a member is required.</summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-internal sealed class RequiredMemberAttribute : Attribute { }
+internal sealed class RequiredMemberAttribute : Attribute
+{
+}
+
+// Copied from:
+// https://github.com/dotnet/runtime/blob/fdd104ec5e1d0d2aa24a6723995a98d0124f724b/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/CompilerFeatureRequiredAttribute.cs
 
 /// <summary>
 /// Indicates that compiler support for a particular feature is required for the location where this attribute is applied.
@@ -17,6 +23,7 @@ internal sealed class RequiredMemberAttribute : Attribute { }
 [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
 internal sealed class CompilerFeatureRequiredAttribute : Attribute
 {
+    /// <nodoc />
     public CompilerFeatureRequiredAttribute(string featureName)
     {
         FeatureName = featureName;
@@ -31,5 +38,28 @@ internal sealed class CompilerFeatureRequiredAttribute : Attribute
     /// If true, the compiler can choose to allow access to the location where this attribute is applied if it does not understand <see cref="FeatureName"/>.
     /// </summary>
     public bool IsOptional { get; init; }
+
+    /// <summary>
+    /// The <see cref="FeatureName"/> used for the ref structs C# feature.
+    /// </summary>
+    public const string RefStructs = nameof(RefStructs);
+
+    /// <summary>
+    /// The <see cref="FeatureName"/> used for the required members C# feature.
+    /// </summary>
+    public const string RequiredMembers = nameof(RequiredMembers);
 }
+
+// Copied from:
+// https://github.com/dotnet/runtime/blob/fdd104ec5e1d0d2aa24a6723995a98d0124f724b/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/SetsRequiredMembersAttribute.cs
+
+/// <summary>
+/// Specifies that this constructor sets all required members for the current type, and callers
+/// do not need to set any required members themselves.
+/// </summary>
+[AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+internal sealed class SetsRequiredMembersAttribute : Attribute
+{
+}
+
 #endif

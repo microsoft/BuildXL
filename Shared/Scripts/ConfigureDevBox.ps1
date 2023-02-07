77b6ef5b30af88f56976ee09402036c01948de24
Michael Pysson <mpysson@microsoft.com>
2023-02-07 00:46:36 +0000

    Merged PR 700066: Add script for repo specific customizations to DevBox image
    Add script for repo specific customizations to DevBox image

---
+++ Shared/Scripts/ConfigureDevBox.ps1
@@ -0,0 +1,7 @@
+# This file should only contain DevBox customizations specific to the BuildXL repo.
+
+# Anything for more general development not coupled to the BuildXL codebase should
+# be specified directly in the DevBox image definition.
+
+# install the BuildXL VSCode extension
+code --install-extension BuildXL.dscript

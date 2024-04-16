67a24e958a904a4fbdeccca2482815396bc291fd
Sahiti Chandramouli <sahitic@microsoft.com>
2024-04-16 18:34:05 +0000

    Merged PR 779690: Changes to modify the version of MSVC according to the image
    Changes to modify the version of MSVC according to the image,
    Turned off the autobaseline feature as well.
    
    The MSVC version has been changed to 14.39
    https://dev.azure.com/mseng/Domino/_build/results?buildId=27811495&view=logs&j=96137153-191f-5184-c736-a57cf0a30600&t=56fa6f3b-b8ab-59b3-f487-fb3f4379b561&l=13
    
    The image has also been changed to
    https://dev.azure.com/mseng/Domino/_build/results?buildId=27811495&view=logs&j=96137153-191f-5184-c736-a57cf0a30600&t=2259007f-c779-4925-9193-8f35d93fa57a&l=10
    
    Related work items: #2169518

--- Shared/Scripts/SetMsvcVersion.ps1
+++ Shared/Scripts/SetMsvcVersion.ps1
@@ -1,17 +1,39 @@
 ## Sets the MSVC Version to be used the VisualCpp SDK in ADO
 
-$InstalledMsvcVersions=Get-ChildItem -Path 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC' -Directory -Name
+# Define possible paths for Visual Studio installations.
+$vsPaths = @(
+    'C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC',
+    'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC'
+)
 
-$PIPELINE_MSVC_VER=$null
+# We always prefer to use the 2022 version of Visual Studio since we need it for resolving few violations raised by the BinSkim tool.
+$selectedPath = $null
 
-if ( $InstalledMsvcVersions -is [system.array] )
-{
-    # If multiple versions are found, then set to the last one
-    $PIPELINE_MSVC_VER=$InstalledMsvcVersions[$InstalledMsvcVersions.Length-1]
+foreach ($path in $vsPaths) {
+    if (Test-Path $path) {
+        $selectedPath = $path
+        break
+    }
 }
-else
-{
-    $PIPELINE_MSVC_VER=$InstalledMsvcVersions
+
+$PIPELINE_MSVC_VER = $null
+
+if ($selectedPath -ne $null) {
+    $InstalledMsvcVersions = Get-ChildItem -Path $selectedPath -Directory -Name
+
+    if ($InstalledMsvcVersions -is [system.array]) {
+        # If multiple versions are found, then set to the last one
+        $PIPELINE_MSVC_VER = $InstalledMsvcVersions[$InstalledMsvcVersions.Length - 1]
+    } 
+    else {
+        $PIPELINE_MSVC_VER = $InstalledMsvcVersions
+    }
+    # Set the pipeline variable to the found MSVC version
+    Write-Host "##vso[task.setvariable variable=MSVC_VERSION;]$PIPELINE_MSVC_VER"
+} 
+else {
+    Write-Error "No valid Visual Studio installations found in the specified paths."
+    exit -1
 }
 
-Write-Host "##vso[task.setvariable variable=MSVC_VERSION;]$PIPELINE_MSVC_VER"
+Write-Host "Setting MSVC Version to: $PIPELINE_MSVC_VER"

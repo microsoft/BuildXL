2102223d06bb8d3fe739d253de84113311f92a94
BuildXL-Pipelines <BuildXL-Pipelines>
2025-03-13 20:34:01 +0000

    Merged PR 832659: Update BuildXL LKG Version to 0.1.0-20250311.3 [skip ci]

--- Shared/Scripts/BuildXLLkgVersionPublic.cmd
+++ Shared/Scripts/BuildXLLkgVersionPublic.cmd
@@ -1,3 +1,3 @@
 set BUILDXL_LKG_NAME=Microsoft.BuildXL.win-x64
-set BUILDXL_LKG_VERSION=0.1.0-20250306.1
+set BUILDXL_LKG_VERSION=0.1.0-20250311.3
 set BUILDXL_LKG_FEED_1=https://pkgs.dev.azure.com/mseng/PipelineTools/_packaging/BuildXL.External/nuget/v3/index.json

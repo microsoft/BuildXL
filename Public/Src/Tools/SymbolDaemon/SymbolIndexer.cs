f07b8fb413b01501600aa4f24c3286708d3a2e39
Kai Chen <Kai.Chen@microsoft.com>
2025-05-08 20:29:10 +0000

    Merged PR 843941: Index the linux debug symbol correctly
    when index linux symbols we should pass alternative 'filename', becasue it won't return expected clientkey
    `_.debug\elf-build-sym-xxxx\_.debug`
    
    Related work items: #2277075

--- Public/Src/Tools/SymbolDaemon/SymbolIndexer.cs
+++ Public/Src/Tools/SymbolDaemon/SymbolIndexer.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.ContractsLight;
+using System.Linq;
 using System.IO;
 using BuildXL.Native.IO;
 using Microsoft.VisualStudio.Services.Content.Common.Tracing;
@@ -17,6 +19,7 @@ namespace Tool.SymbolDaemon
     {
         private readonly SymstoreUtil m_symstoreUtil;
         private readonly Microsoft.VisualStudio.Services.Symbol.App.Indexer.IndexerUtil m_indexerUtil;
+        private const string c_invalidClientKey = "/00000000000000000000000000000000/";
 
         /// <nodoc />
         public SymbolIndexer(IAppTraceSource tracer)
@@ -38,7 +41,11 @@ namespace Tool.SymbolDaemon
                 new[] { file.FullName },
                 calculateBlobId,
                 // Currently, only file-deduped (VsoHash) symbols are supported.
-                isChunked: false));
+                isChunked: false)
+                // SymstoreUtil can interpret a Linux debug symbol file as a .pdb file and attempt to extract a key out of it.
+                // In such a case, the extraction will fail but it will be a silent failure and SymstoreUtil just returns a string of zeros.
+                // We need to check and skip all entries with invalid keys.
+                .Where(entry => entry.ClientKey.IndexOf(c_invalidClientKey) == -1));
 
             // If we get no entries from the symstore, it might mean that the file is not supported by the symstore.
             // In that case, we try to get entries using the IndexerUtil. The symstore is used first because it always
@@ -61,7 +68,10 @@ namespace Tool.SymbolDaemon
                     entries.AddRange(m_indexerUtil.GetDebugEntryData(
                         tempFileName,
                         // We need to pass the original file name to the IndexerUtil, because it uses it to create the client key.
-                        new[] { file.FullName },
+                        // Currently, there is a bug in the key generation for debug symbols (file extension .dbg/.debug) in IndexerUtil.
+                        // To work around it, in case of debug symbols, we don't pass the file name at all. It's fine to do this because
+                        // the clientkeys for debug symbols do not include file name, they are always in _.debug\elf - build - sym - xxxx\_.debug format.
+                        (file.FullName.EndsWith(".debug", StringComparison.OrdinalIgnoreCase) || file.FullName.EndsWith(".dbg", StringComparison.OrdinalIgnoreCase)) ? null : new[] { file.FullName },
                         calculateBlobId,
                         // Currently, only file-deduped (VsoHash) symbols are supported
                         isChunked: false));
